46070b7ecf60c3a0b88e0817490f2b45092670aa
Ken Caron <ken.caron@gmail.com>
2016-09-04 08:33:20 -0600

    cloudpebble working with clay

---
+++ src/pkjs/config.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "Watchface Configuration"
+  },
+  {
+    "type": "text",
+    "defaultValue": "Clay with Rocky.js"
+  },
+  {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Colors"
+      },
+      {
+        "type": "color",
+        "appKey": "BackgroundColor",
+        "defaultValue": "0x000000",
+        "label": "Background Color"
+      },
+      {
+        "type": "color",
+        "appKey": "ForegroundColor",
+        "defaultValue": "0xFFFFFF",
+        "label": "Foreground Color"
+      }
+    ]
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Save Settings"
+  }
+];
--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,5 +1,13 @@
 var rocky = require('rocky');
 
+
+var settings = null;
+
+var defaultSettings = {
+  ForegroundColor: 'black',
+  BackgroundColor: 'white'
+};
+
 rocky.on('draw', function(event) {
   // Get the CanvasRenderingContext2D object
   var ctx = event.context;
@@ -10,23 +18,38 @@ rocky.on('draw', function(event) {
   // Determine the width and height of the display
   var w = ctx.canvas.unobstructedWidth;
   var h = ctx.canvas.unobstructedHeight;
+  
+  var foregroundColor = cssColor(defaultSettings.ForegroundColor);
+  var backgroundColor = cssColor(defaultSettings.BackgroundColor);
 
+  if (settings) {
+    foregroundColor = cssColor(settings.ForegroundColor);
+    backgroundColor = cssColor(settings.BackgroundColor);
+  }
+  
   // Current date/time
   var d = new Date();
 
   // Set the text color
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = backgroundColor;
   
   //set the bg color
   ctx.fillRect(0, 0, w, h);
 
   
-  ctx.fillStyle = 'black';
+  ctx.fillStyle = foregroundColor;
   // Center align the text
   ctx.textAlign = 'center';
 
   // Display the time, in the middle of the screen
   ctx.fillText(d.toLocaleTimeString(), w / 2, h / 2, w);
+});
+
+rocky.on('message', function(event) {
+  console.log('message', JSON.stringify(event));
+  console.log('data', JSON.stringify(event.data));
+  settings = event.data;
+  rocky.requestDraw();
 });
 
 rocky.on('minutechange', function(event) {
@@ -36,3 +59,41 @@ rocky.on('minutechange', function(event) {
   // Request the screen to be redrawn on next pass
   rocky.requestDraw();
 });
+
+rocky.postMessage({command: 'settings'});
+
+
+
+
+
+// Borrowed from Clay.js
+
+/**
+ * @param {string|boolean|number} color
+ * @returns {string}
+ */
+function cssColor(color) {
+  if (typeof color === 'number') {
+    color = color.toString(16);
+  } else if (!color) {
+    return 'transparent';
+  }
+
+  color = padColorString(color);
+
+  return '#' + color;
+}
+
+/**
+ * @param {string} color
+ * @return {string}
+ */
+function padColorString(color) {
+  color = color.toLowerCase();
+
+  while (color.length < 6) {
+    color = '0' + color;
+  }
+
+  return color;
+}
